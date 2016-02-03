078c0c8a5fdb01c412283ebf2a57ed4e9d0625ae
micnori <micnori@yahoo.com>
2016-02-03 11:38:08 +0100

    dashboard: first import

---
+++ sco.climb.game-dashboard/src/main/java/it/smartcommunitylab/climb/gamification/dashboard/common/Const.java
@@ -0,0 +1,6 @@
+package it.smartcommunitylab.climb.gamification.dashboard.common;
+
+public class Const {
+	public static final String ERRORTYPE = "errorType";
+	public static final String ERRORMSG = "errorMsg";
+}
---
+++ sco.climb.game-dashboard/src/main/java/it/smartcommunitylab/climb/gamification/dashboard/storage/RepositoryManager.java
@@ -0,0 +1,100 @@
+package it.smartcommunitylab.climb.gamification.dashboard.storage;
+
+import it.smartcommunitylab.climb.gamification.dashboard.security.DataSetInfo;
+import it.smartcommunitylab.climb.gamification.dashboard.security.Token;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+
+public class RepositoryManager {
+	private static final transient Logger logger = LoggerFactory.getLogger(RepositoryManager.class);
+	
+	private MongoTemplate mongoTemplate;
+	private String defaultLang;
+	
+	public RepositoryManager(MongoTemplate template, String defaultLang) {
+		this.mongoTemplate = template;
+		this.defaultLang = defaultLang;
+	}
+	
+	public String getDefaultLang() {
+		return defaultLang;
+	}
+
+	public Token findTokenByToken(String token) {
+		Query query = new Query(new Criteria("token").is(token));
+		Token result = mongoTemplate.findOne(query, Token.class);
+		return result;
+	}
+	
+	public List<DataSetInfo> getDataSetInfo() {
+		List<DataSetInfo> result = mongoTemplate.findAll(DataSetInfo.class);
+		return result;
+	}
+	
+	public void saveDataSetInfo(DataSetInfo dataSetInfo) {
+		Query query = new Query(new Criteria("ownerId").is(dataSetInfo.getOwnerId()));
+		DataSetInfo appInfoDB = mongoTemplate.findOne(query, DataSetInfo.class);
+		if (appInfoDB == null) {
+			mongoTemplate.save(dataSetInfo);
+		} else {
+			Update update = new Update();
+			update.set("password", dataSetInfo.getPassword());
+			update.set("token", dataSetInfo.getToken());
+			mongoTemplate.updateFirst(query, update, DataSetInfo.class);
+		}
+	}
+	
+	public void saveAppToken(String name, String token) {
+		Query query = new Query(new Criteria("name").is(name));
+		Token tokenDB = mongoTemplate.findOne(query, Token.class);
+		if(tokenDB == null) {
+			Token newToken = new Token();
+			newToken.setToken(token);
+			newToken.setName(name);
+			newToken.getPaths().add("/api");
+			mongoTemplate.save(newToken);
+		} else {
+			Update update = new Update();
+			update.set("token", token);
+			mongoTemplate.updateFirst(query, update, Token.class);
+		}
+	}
+	
+	public List<?> findData(Class<?> entityClass, Criteria criteria, Sort sort, String ownerId)
+			throws ClassNotFoundException {
+		Query query = null;
+		if (criteria != null) {
+			query = new Query(new Criteria("ownerId").is(ownerId).andOperator(criteria));
+		} else {
+			query = new Query(new Criteria("ownerId").is(ownerId));
+		}
+		if (sort != null) {
+			query.with(sort);
+		}
+		query.limit(5000);
+		List<?> result = mongoTemplate.find(query, entityClass);
+		return result;
+	}
+
+	public <T> T findOneData(Class<T> entityClass, Criteria criteria, String ownerId)
+			throws ClassNotFoundException {
+		Query query = null;
+		if (criteria != null) {
+			query = new Query(new Criteria("ownerId").is(ownerId).andOperator(criteria));
+		} else {
+			query = new Query(new Criteria("ownerId").is(ownerId));
+		}
+		T result = mongoTemplate.findOne(query, entityClass);
+		return result;
+	}
+
+
+}
