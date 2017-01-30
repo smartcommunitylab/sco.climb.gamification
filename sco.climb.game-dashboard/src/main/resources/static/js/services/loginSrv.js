1ac3debded51545129cb2e070ae4704712ae5470
chin8 <matteo.chini@fbk.eu>
2017-01-30 13:39:52 +0100

    new dialog on calendar

--- sco.climb.game-dashboard/src/main/resources/static/js/services/loginSrv.js
+++ sco.climb.game-dashboard/src/main/resources/static/js/services/loginSrv.js
@@ -1,6 +1,17 @@
 angular.module('climbGame.services.login', [])
   .factory('loginService', function () {
     var loginService = {};
-
+    loginService.getOwnerId = function () {
+      return 'VELA';
+    }
+    loginService.getGameId = function () {
+      return '588889c0e4b0464e16ac40a0';
+    }
+    loginService.getUserToken = function () {
+      return 'e2lEbKBXKE04CoS1';
+    }
+    loginService.getClassRoom = function () {
+      return '1^';
+    }
     return loginService;
   });
