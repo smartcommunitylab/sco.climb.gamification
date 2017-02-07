533f66ed575ec08935b78281a3bf1743942726b7
chin8 <matteo.chini@fbk.eu>
2017-02-07 14:53:00 +0100

    fixed style on login and classes

--- sco.climb.game-dashboard/src/main/resources/static/js/controllers/classSelectionCtrl.js
+++ sco.climb.game-dashboard/src/main/resources/static/js/controllers/classSelectionCtrl.js
@@ -1,19 +1,30 @@
 /* global angular */
 angular.module('climbGame.controllers.classSelection', [])
-  .controller('classSelectionCtrl', ["$scope", "$state", "loginService", function ($scope, $state, loginService) {
+  .controller('classSelectionCtrl', ["$scope", "$state", "$mdToast", "$filter", "loginService", function ($scope, $state, $mdToast, $filter, loginService) {
     $scope.classes = loginService.getAllClasses();
     $scope.formClasses = [];
+    $scope.labelClasses = [
+                   "Prima",
+                   "Seconda",
+                   "Terza",
+                   "Quarta",
+                   "Quinta"];
     for (var i = 0; i < $scope.classes.length; i++) {
       $scope.formClasses.push({
         id: $scope.classes[i],
-        value: $scope.classes[i]
+        value: $scope.classes[i],
+        label: $scope.labelClasses[i]
       });
 
     }
     $scope.selectedClass = null;;
 
     $scope.login = function () {
-      loginService.setClassRoom($scope.selectedClass.id);
-      $state.go('home')
+      if ($scope.selectedClass) {
+        loginService.setClassRoom($scope.selectedClass.id);
+        $state.go('home')
+      } else {
+        $mdToast.show($mdToast.simple().content($filter('translate')('class_choose_room')));
+      }
     }
   }])
